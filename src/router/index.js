19d57993406bfde11238cfc53a6100a91f8494ae
Claire <claire210401@gmail.com>
2024-05-08 23:22:40 +0800

    fix: 🐛 delete front router

--- src/router/index.js
+++ src/router/index.js
@@ -15,11 +15,11 @@ const routes = [
         path: '',
         component: () => import('../views/HomeView.vue')
       },
-      {
-        path: 'cart',
-        name: '購物車',
-        component: () => import('../views/CartView.vue')
-      },
+      // {
+      //   path: 'cart',
+      //   name: '購物車',
+      //   component: () => import('../views/CartView.vue')
+      // },
       {
         path: 'products',
         name: '產品列表',
@@ -33,10 +33,10 @@ const routes = [
     component: () => import('../views/dashboard/DashboardView.vue'),
     // 前台巢狀路由
     children: [
-      {
-        path: 'orders',
-        component: () => import('../components/dashboard/OrdersComponent.vue')
-      },
+      // {
+      //   path: 'orders',
+      //   component: () => import('../components/dashboard/OrdersComponent.vue')
+      // },
       {
         path: 'products',
         component: () =>
